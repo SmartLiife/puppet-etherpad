5ecea3133ea86731d880fbb997ea2c76c14bff4d
your1p <smartliife.fr>
2018-06-27 14:44:44 +0200

    Mise à jour de la branche

--- types/stpadoptions.pp
+++ types/stpadoptions.pp
@@ -1,15 +1,15 @@
 # Matches Pad Options Keys And Values
 type Etherpad::Stpadoptions = Struct[
   {
-    Optional['nocolors']         => Variant[Boolean, String],
-    Optional['showcontrols']     => Variant[Boolean, String],
-    Optional['showlinenumbers']  => Variant[Boolean, String],
-    Optional['usemonospacefont'] => Variant[Boolean, String],
-    Optional['username']         => Variant[Boolean, String],
-    Optional['usercolor']        => Variant[Boolean, String],
-    Optional['rtl']              => Variant[Boolean, String],
-    Optional['alwaysshowchat']   => Variant[Boolean, String],
-    Optional['chatandusers']     => Variant[Boolean, String],
+    Optional['nocolors']         => Boolean,
+    Optional['showcontrols']     => Boolean,
+    Optional['showlinenumbers']  => Boolean,
+    Optional['usemonospacefont'] => Boolean,
+    Optional['username']         => Boolean,
+    Optional['usercolor']        => Boolean,
+    Optional['rtl']              => Boolean,
+    Optional['alwaysshowchat']   => Boolean,
+    Optional['chatandusers']     => Boolean,
     Optional['lang']             => String[1],
   }
 ]
